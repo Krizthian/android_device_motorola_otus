5ccc39df00cb4c2eb1efdffaf46219285d482161
Cristian Ortiz <cristianortizmorales2013@gmail.com>
2015-09-30 12:46:16 -0500

    otus: Final build fix

--- xpe.mk
+++ xpe.mk
@@ -4,6 +4,6 @@ $(call inherit-product, device/motorola/otus/full_otus.mk)
 $(call inherit-product, vendor/XPe/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO E
-PRODUCT_NAME := xpe_falcon
+PRODUCT_NAME := xpe_otus
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
